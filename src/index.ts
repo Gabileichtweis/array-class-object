86842f40f05337de4bbee1784132cd34bff99d9d
Gabriela Leichtweis <112829072+Gabileichtweis@users.noreply.github.com>
2023-05-15 23:43:35 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,23 @@ function atividade1() {
   const listaNumeros: number[] = [
     8, 72, 1, 37, 21, 54, 7, 81, 23, 92, 12, 87, 3, 12, 6, 4,
   ];
-  const novaLista: number[] = [];
+  const novaLista: number[] = listaNumeros.filter((numero) => numero % 2 !== 0);
+
+  console.log(novaLista);
 }
-atividade1();
 
 function atividade2() {
   const listaNumeros2: number[] = [
     8, 72, 1, 37, 21, 54, 7, 81, 23, 92, 12, 87, 3, 12, 6, 4,
   ];
+
+  //reduce => retorna 1 único valor
+  const soma: number = listaNumeros2.reduce(
+    (soma, numero) => (soma += numero),
+    0
+  );
+  console.log(soma);
 }
-atividade2();
 
 function atividade3() {}
 atividade3();
